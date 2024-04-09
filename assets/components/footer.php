2fb41b6e8bd81ba59dce4d4d4716332a85d14742
Zulukhadeen <123221930+Zulukhadeen@users.noreply.github.com>
2024-04-09 18:01:39 +0100

    content edited
    changed text content in the nav bar and footer area

--- assets/components/footer.php
+++ assets/components/footer.php
@@ -3,16 +3,17 @@
     <!-- <div class="max-w-screen-xl px-4 py-12 mx-auto space-y-8 overflow-hidden sm:px-6 lg:px-8"> -->
         <div class="">
             <a class="logo ff-jua " href="./index.html">
-                <span class="text-dark"> IBILE </span> <span class="text-secondary">FOOD</span>
+                <span class="text-dark"> African Cuisine </span>
+                 <span class="text-secondary">(IBILE)</span>
             </a>
             <p> Looking for a place to get African recipe. <br /> IBILIE is your stop </p>               
         </div>
         <div>
-            <li> <a href=""> South African Dishes</a> </li>
-            <li> <a href=""> West African Dishes</a> </li>
-            <li> <a href=""> East African Dishes</a> </li>
             <li> <a href=""> African Main Dishes</a> </li>
-
+            <li> <a href=""> West African Dishes</a> </li>
+            <li> <a href=""> South African Dishes</a> </li>
+            <li> <a href=""> North African Dishes</a> </li>
+            <li> <a href=""> East African Dishes</a> </li>
         </div>
         <div>
             <li> <a href=""> About Us</a> </li>

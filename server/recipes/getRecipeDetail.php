7a9fab921bd4d986f51be488513fc080e8cdbf96
Zulukhadeen <123221930+Zulukhadeen@users.noreply.github.com>
2024-04-12 13:09:42 +0100

    fix recipe detail scripts

--- server/recipes/getRecipeDetail.php
+++ server/recipes/getRecipeDetail.php
@@ -1,12 +1,13 @@
 <?php
     require_once('../server-config.php');
     
-    $recipe_id = isset($_GET['recipe_id']) && $_GET['recipe_id'];
+    $recipe_id = isset($_GET['recipe_id']) ? $_GET['recipe_id'] : "null";
     $res = $connection->query(query: "SELECT recipe_id, name, description, image, location, category, ingredients, instructions from recipes WHERE recipe_id='$recipe_id'");
+
     if ($res->num_rows > 0 ) {
         $row=$res->fetch_assoc();
         echo json_encode($row);
     } else {
-        echo json_encode(array('mesage' => "error"));
+        echo json_encode(array('mesage' => $recipe_id));
     }
 ?>
